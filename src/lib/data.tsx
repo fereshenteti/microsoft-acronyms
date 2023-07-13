412a7edb6500c9f03f909ab1b472f08cc3ddb353
fereshenteti <fereshenteti@gmail.com>
2023-07-13 16:37:39 +0200

    completed search box for Microsoft Acronyms

---
+++ src/lib/data.tsx
@@ -0,0 +1,92 @@
+"use client";
+
+export interface DataType {
+    key: string;
+    name: string;
+}
+
+export const data = [
+    {
+        key: "ACO",
+        name: "Azure Credit Offer",
+    },
+    {
+        key: "ACR",
+        name: "Azure Container Registry",
+    },
+    {
+        key: "ACS", 
+        name: "Azure Communication Services",
+    },
+    {
+        key: "ACV", 
+        name: "Annual Contractual Value",
+    },
+    {
+        key: "ADAL (Legacy)", 
+        name: "Azure Active Directory Authentication Library",
+    },
+    {
+        key: "AE", 
+        name: "Account Executive",
+    },
+    {
+        key: "AIP (Legacy)", 
+        name: "Azure Information Protection",
+    },
+    {
+        key: "AKS", 
+        name: "Azure Kubernetes Service",
+    },
+    {
+        key: "ALM", 
+        name: "Application Lifecycle Management",
+    },
+    {
+        key: "ALT", 
+        name: "Azure Load Testing",
+    },
+    {
+        key: "AMM", 
+        name: "Azure Migration and Modernization",
+    },
+    {
+        key: "AOG-G", 
+        name: "Agreement for Online Services for Government",
+    },
+    {
+        key: "ARR", 
+        name: "Azure Rapid Response"
+    }
+]
+
+// export const data = [
+//     {
+//         "key": "FH",
+//         "name": "Feres Henteti"
+//     },
+//     {
+//         "key": "BB",
+//         "name": "Boulogne Billancourt"
+//     },
+//     {
+//         "key": "ABA",
+//         "name": "Achraf Ben Alaya"
+//     },
+//     {
+//         "key": "SSBB",
+//         "name": "Super Smash Bros Brawl"
+//     },
+//     {
+//         "key": "HTML",
+//         "name": "Hyper Text Markup Language"
+//     },
+//     {
+//         "key": "TFSI",
+//         "name": "Turbo Fuel System Injection"
+//     },
+//     {
+//         "key": "ADM",
+//         "name": "Azure Data Management"
+//     }
+// ]
